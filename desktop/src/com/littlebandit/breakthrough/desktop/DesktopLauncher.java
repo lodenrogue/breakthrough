4f2bb963cfd8a719e073cba484afecfdcacc764f
Miguel Hernandez <phyzix5761@gmail.com>
2014-10-28 19:49:11 -0400

    Initial commit

---
+++ desktop/src/com/littlebandit/breakthrough/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.littlebandit.breakthrough.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.littlebandit.breakthrough.Breakthrough;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Breakthrough(), config);
+	}
+}
