de32a58901f881535408dd4510dfc83c4bb51f1a
Miguel Hernandez <phyzix5761@gmail.com>
2014-10-28 20:05:48 -0400

    JRE and state changes
    Changed JRE to 1.7 instead of 1.8
    Added game state package

---
+++ core/src/com/littlebandit/breakthrough/states/State.java
@@ -0,0 +1,54 @@
+package com.littlebandit.breakthrough.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * State class offers abstract methods for derivative states to be used in a
+ * state based game. Takes in a GameStateManager in constructor to handle state
+ * changes.
+ * 
+ * @author Miguel Hernandez
+ *
+ */
+public abstract class State {
+	protected GameStateManager gameStateManager;
+
+	public State(GameStateManager gsm) {
+		gameStateManager = gsm;
+	}
+
+	/**
+	 * Called when the state is created. Put all set up here.
+	 */
+	public abstract void create();
+
+	/**
+	 * Logic update method.
+	 */
+	public abstract void update();
+
+	/**
+	 * Called to render graphics.
+	 * 
+	 * @param batch SpriteBatch used to render components.
+	 */
+	public abstract void render(SpriteBatch batch);
+
+	/**
+	 * This method is called in the GameStateManager to both update and
+	 * render this state.
+	 * 
+	 * @param batch
+	 */
+	public final void updateAndRender(SpriteBatch batch) {
+		update();
+		render(batch);
+	}
+
+	/**
+	 * Called when resources need to be released. Typically, when this state
+	 * is being removed from the stack.
+	 */
+	public abstract void dispose();
+
+}
--- desktop/src/com/littlebandit/breakthrough/desktop/DesktopLauncher.java
+++ desktop/src/com/littlebandit/breakthrough/desktop/DesktopLauncher.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.littlebandit.breakthrough.Breakthrough;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Breakthrough.VIRTUAL_WIDTH;
+		config.height = Breakthrough.VIRTUAL_HEIGHT;
+		config.title = Breakthrough.TITLE;
 		new LwjglApplication(new Breakthrough(), config);
 	}
 }
