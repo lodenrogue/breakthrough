da28ffaf86357196175c982ce9e79e8ec196a0ad
Ashish <bashish1989@gmail.com>
2014-10-30 22:53:58 +0530

    Added extends Game, Temp Enum for Game State

--- core/src/com/littlebandit/breakthrough/Breakthrough.java
+++ core/src/com/littlebandit/breakthrough/Breakthrough.java
@@ -1,6 +1,7 @@
 package com.littlebandit.breakthrough;
 
-import com.badlogic.gdx.ApplicationAdapter;
+
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -11,14 +12,20 @@ import com.littlebandit.breakthrough.gameutilities.GameManager;
 import com.littlebandit.breakthrough.states.GameStateManager;
 import com.littlebandit.breakthrough.states.PlayState;
 
-public class Breakthrough extends ApplicationAdapter {
+public class Breakthrough extends Game {
 	public static final int VIRTUAL_WIDTH = 800;
 	public static final int VIRTUAL_HEIGHT = 600;
 	public static final float PIXELS_PER_METER = 15f;
 	public static final String TITLE = "Breakthrough";
 	public static FitViewport viewport;
 
-	private World world;
+    public enum State{ //to set the game state
+        Running, Paused, Resumed
+    }
+
+    private State state = State.Running;
+
+    private World world;
 	private SpriteBatch batch;
 	private GameStateManager gsm;
 
@@ -41,18 +48,28 @@ public class Breakthrough extends ApplicationAdapter {
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		batch.begin();
-		gsm.render(batch);
-		batch.end();
+            Gdx.gl.glClearColor(0, 0, 0, 1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+            batch.begin();
+            gsm.render(batch);
+            batch.end();
+
 	}
 	
 	@Override
 	public void pause(){
-		Gdx.app.exit();
+        state = State.Paused; //to not kill the game on pause or minimizing window
+        Gdx.app.log("pause","On pause");
+        //Gdx.app.exit();
+        //TODO Save game state when pause is invoked and resume when resume is invoked
 	}
+
+    public void resume(){
+    state = State.Resumed;
+        //render();
+    }
 
 	@Override
 	public void dispose() {
